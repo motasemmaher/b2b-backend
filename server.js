8b21ce1f45849987d1dd48d0e32f07674531b233
motasem salem <mmsalem17@cit.just.edu.jo>
2021-01-24 21:52:46 +0200

    complete-chat

--- server.js
+++ server.js
@@ -3,43 +3,40 @@ const connection = require('./connect');
 const Permissions = require('./src/business/Permissions/Permissions');
 const permissions = new Permissions();
 const PORT = 3000;
+const chat = require('./src/business/Objects').CHAT;
 
 let http = require('http');
 let server = http.Server(app);
 
 // let socketIO = require('socket.io');
 // let io = socketIO(server, );
-
-
 let {
     userForChat
-} = require('./chat');
-userForChat = userForChat || [];
-// let userForChat = getUserForChat();
+} = require('./src/routes/Chat');
+userForChat = userForChat;
+
+const io = require('./socket').init(server);
+io.on('connection', (socket) => {
+    userForChat.forEach(Element => {
+        socket.on(Element, (message) => {
+            chat.pushMessage(Element, message).then(result => {
+                socket.broadcast.emit(Element, message);
+            }).catch(err => {
+                if (err) {
+                    // console.log(err)
+                    res.status(400).send({
+                        error: err
+                    });
+                }
+            });
+        });
+    });
+});
+
 connection.connect()
     .then(() => {
         server.listen(PORT, () => {
-            // io.on('connection', (socket) => {
-            //     console.log('user connected');
-            // });
-            const io = require('./socket').init(server);
-            io.on('connection', (socket) => {
-                // console.log('connected', socket)
-                userForChat.forEach(Element => {
-                    socket.on(Element, (message) => {
-                        chat.pushMessage(Element, message).then(result => {
-                            socket.broadcast.emit(Element, message);
-                        }).catch(err => {
-                            if (err) {
-                                // console.log(err)
-                                res.status(400).send({
-                                    error: err
-                                });
-                            }
-                        });
-                    });
-                });
-            });
+           
             Promise.all([
                 permissions.createPermissions('carOwner'),
                 permissions.createPermissions('garageOwner'),
