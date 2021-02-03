c5104d0b61aad2ed539837af9b06f8e113ce4f3f
motasem salem <mmsalem17@cit.just.edu.jo>
2021-02-03 23:39:27 +0200

    remove

--- connect.js
+++ connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://admin:admin@cluster0.n4bjd.mongodb.net/GP?retryWrites=true&w=majority";
+const uri = "mongodb+srv://admin:admin@cluster0.n4bjd.mongodb.net/B2B?retryWrites=true&w=majority";
 const testuri = "mongodb+srv://admin:admin@cluster0.n4bjd.mongodb.net/GPTest?retryWrites=true&w=majority";
 // const uri = 'mongodb://localhost:27017/GPTestv3';
 //const testuri = 'mongodb://localhost:27017/GPTest';
@@ -7,49 +7,46 @@ const testuri = "mongodb+srv://admin:admin@cluster0.n4bjd.mongodb.net/GPTest?ret
 //"test": "nyc --reporter=text --timeout 100000 mocha --exit"
 function connect() {
   return new Promise((resolve, reject) => {
-    if (process.env.NODE_ENV === 'test') 
-    {
-        mongoose.connect(testuri,{ useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true, useFindAndModify: false})
+    if (process.env.NODE_ENV === 'test') {
+      mongoose.connect(testuri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
         .then((res, err) => {
-            console.log("connected to testdb")
-            require ('./src/models/model/Admin');
-            require ('./src/models/model/Car');
-            require ('./src/models/model/CarOwner');
-            require ('./src/models/model/CartItem');
-            require ('./src/models/model/Category');
-            require ('./src/models/model/Complaint');
-            require ('./src/models/model/Contact');
-            require ('./src/models/model/GarageOwner');
-            require ('./src/models/model/Menu');
-            require ('./src/models/model/Message');
-            require ('./src/models/model/Offer');
-            require ('./src/models/model/Order');
-            require ('./src/models/model/Product');
-            require ('./src/models/model/ShoppingCart');
-            require ('./src/models/model/Store');
-            require ('./src/models/model/User');
-            require ('./src/models/model/Warehouse');
+          console.log("connected to testdb")
+          require('./src/models/model/Admin');
+          require('./src/models/model/Car');
+          require('./src/models/model/CarOwner');
+          require('./src/models/model/CartItem');
+          require('./src/models/model/Category');
+          require('./src/models/model/Complaint');
+          require('./src/models/model/Contact');
+          require('./src/models/model/GarageOwner');
+          require('./src/models/model/Menu');
+          require('./src/models/model/Message');
+          require('./src/models/model/Offer');
+          require('./src/models/model/Order');
+          require('./src/models/model/Product');
+          require('./src/models/model/ShoppingCart');
+          require('./src/models/model/Store');
+          require('./src/models/model/User');
+          require('./src/models/model/Warehouse');
 
-            if (err) return reject(err);
-            resolve();
+          if (err) return reject(err);
+          resolve();
         })
-        .catch(err => console.log("Error connecting to testdb"));    
+        .catch(err => console.log("Error connecting to testdb"));
     }
-    else 
-    {
-        mongoose.connect(uri,{ useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true, useFindAndModify: false})
+    else {
+      mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false })
         .then((res, err) => {
           console.log("connected to DB")
           if (err) return reject(err);
           resolve();
-      })
-      .catch(err => console.log("Error connecting to db"));    
+        })
+        .catch(err => console.log("Error connecting to db"));
     }
   });
 }
 
-function close()
-{
+function close() {
   return mongoose.disconnect();
 }
 
